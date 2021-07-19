6c0c03387490b2c6ab958b907a6392432a8d6adf
Geraint Pickard-Skeats <geraintps@gmail.com>
2021-07-19 03:40:10 +0100

    Update t6-range-tracking.js

--- cypress/integration/t6-range-tracking.js
+++ cypress/integration/t6-range-tracking.js
@@ -11,7 +11,7 @@ describe("Tracking", function () {
     function testWithPlaceRanges(testRunner, restOfTest) {
         let mapTest = new MapTest(testRunner);
         cy.window().then(win => {win.map.moveTo(-0.125990, 51.500984, 0, 0);
-            mapTest.addPlaceAtCentreWithPlus((editor) => {
+            mapTest.addPlaceAtCentre((editor) => {
                 editor.textInput("Test place 10m", "ego");
                 cy.get("#author").click();
                 cy.get("#editTrackingRangeItem").click();
@@ -26,7 +26,7 @@ describe("Tracking", function () {
             });
         });
         cy.window().then(win => {win.map.moveTo(-0.124551, 51.500935, 0, 0);
-            mapTest.addPlaceAtCentreWithPlus((editor) => {
+            mapTest.addPlaceAtCentre((editor) => {
                 editor.textInput("Test place 200m", "ego");
                 cy.get("#author").click();
                 cy.get("#editTrackingRangeItem").click();
@@ -46,22 +46,22 @@ describe("Tracking", function () {
     it("Pops nearest place", function () {
         testWithPlaceRanges(this, function (mapTest) {
             cy.window().then(win => { 
-                win.paused = false; 
+                win.tracker.paused = false; 
                 win.updatePosition({ coords: { latitude: 51.500984, longitude: -0.125990 } });
                 mapTest.checkLightBox(0, "Test place 10m");
             })
             cy.window().then(win => {
-                win.paused = false; 
+                win.tracker.paused = false;
                 win.updatePosition({ coords: { latitude: 51.500976, longitude: -0.125853 } });
                 mapTest.checkLightBox(0, "Test place 10m");
             })
             cy.window().then(win => {
-                win.paused = false; 
+                win.tracker.paused = false;
                 win.updatePosition({ coords: { latitude: 51.500935, longitude: -0.124551 } });
                 mapTest.checkLightBox(0, "Test place 200m");
             })
             cy.window().then(win => {
-                win.paused = false; 
+                win.tracker.paused = false; 
                 win.updatePosition({ coords: { latitude: 51.500935, longitude: -0.124551 } });
                 mapTest.checkLightBox(0, "Test place 200m");
             })
