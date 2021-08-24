01723fec2758f95ad4cf2a1fc325f4ee25725728
Geraint Pickard-Skeats <geraintps@gmail.com>
2021-08-24 16:59:56 +0100

    Update

--- r/jsx/App.jsx
+++ r/jsx/App.jsx
@@ -16,7 +16,7 @@ class TopLeft extends React.Component {
       </div>
     }
     return <div className="topLeft">
-    <button onClick={() => this.setState({ signedin: true})}>Sign In</button>
+    <button onClick={() => this.setState({ signedin: true })}>Sign In</button>
     <button>?</button>
     <button>+</button>
     <button>🏠</button>
@@ -64,50 +64,6 @@ class BottomLeft extends React.Component {
   }
 }
 
-class Popup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { popupOpen: false };
-  }
-
-  backgroundDim() {
-    let background = document.getElementById("image");
-    if (this.state.popupOpen) {
-      background.style.opacity = "0.5";
-    } else {
-      background.style.opacity = "1";
-    }
-  }
-
-  render() {
-    this.backgroundDim();
-    if (!this.state.popupOpen) {
-      return <div className="popupButton">
-        <button onClick={() => this.setState({ popupOpen: true })}>Open Popup</button>
-      </div>
-    } else {
-      return <div className="popupButton">
-        <button onClick={() => this.setState({ popupOpen: false })}>Close Popup</button>
-        <div className="popupBox">
-          <button className="tags">Animals</button>
-          <button className="tags">Plants</button>
-          <button className="tags">Rocks</button>
-          <button className="tags">People</button>
-          <button className="tags">Weather</button>
-          <button className="tags">Me</button>
-          <button onClick={() => this.setState({ popupOpen: false })} className="closePopup">X</button>
-          <label>Username</label>
-          <br></br>
-          <br></br>
-          <input className="title" placeholder="Title"></input>
-          <br></br>
-          <br></br>
-          <input className="description" placeholder="Description"></input>
-        </div>
-      </div>
-    }
-  }
-}
 
 
 
@@ -122,4 +78,3 @@ ReactDOM.render(React.createElement(TopRight, null), document.getElementById('to
 
 ReactDOM.render(React.createElement(TopLeft, null), document.getElementById('topLeftControls'));
 
-ReactDOM.render(React.createElement(Popup, null), document.getElementById('popupContainer'));
---
+++ r/jsx/popup.jsx
@@ -0,0 +1,52 @@
+class Popup extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { popupOpen: false };
+    }
+
+  
+    backgroundDim() {
+      let background = document.getElementById("image");
+      if (this.state.popupOpen) {
+        background.style.opacity = "0.5";
+      } else {
+        background.style.opacity = "1";
+      }
+    }
+  
+
+    render() {
+      this.backgroundDim();
+      if (!this.state.popupOpen) {
+        return <div>
+          <button className="openPopup" onClick={() => this.setState({ popupOpen: true })}>Open Popup</button>
+        </div>
+      } else {
+        return <div>
+          <button className="openPopup" onClick={() => this.setState({ popupOpen: false })}>Close Popup</button>
+          <div className="popupBox">
+            <button className="tags">Animals</button>
+            <button className="tags">Plants</button>
+            <button className="tags">Rocks</button>
+            <button className="tags">People</button>
+            <button className="tags">Weather</button>
+            <button className="tags">Me</button>
+            <button onClick={() => this.setState({ popupOpen: false })} className="closePopup">X</button>
+            <label>Username</label>
+            <br></br>
+            <br></br>
+            <input className="title" placeholder="Title"></input>
+            <br></br>
+            <br></br>
+            <input className="description" placeholder="Description"></input>
+          </div>
+        </div>
+      }
+    }
+    
+  }
+
+
+
+
+ReactDOM.render(React.createElement(Popup, null), document.getElementById('popupContainer'));
